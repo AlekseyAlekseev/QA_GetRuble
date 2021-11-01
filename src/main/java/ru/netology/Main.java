05fd2c563b712ddd584fbcce01965ca0167732fd
Aleksey Alekseev <a.alekseev5743@yandex.ru>
2021-11-01 08:40:08 +0300

    last commit

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -5,30 +5,19 @@ public class Main {
 
         int customerBalance = 500; // баланс на счете клиента
 
-        int depositAmount = 2000; // сумма пополнения
+        int depositAmount = 100; // сумма пополнения
         int bonus; // начисляемые бонусы
         int bonusBalance = 7; // баланс бонусного счета
+        int finalBalance = customerBalance + depositAmount; //расчет баланса после пополнения
 
-        int newCustomerBalance = customerBalance + depositAmount; // пополнение счета
-
-
-        if (newCustomerBalance > 1000) {
-            bonus = bonusBalance + newCustomerBalance / 100;
+        if (depositAmount > 1000) {
+            bonus = depositAmount / 100;
             bonusBalance += bonus;
-            System.out.println("Деньги зачислены!\n\nНачислено бонусов: " + bonus + "\nБаланс бонусного счета: " + bonusBalance);
+            System.out.println("Деньги зачислены!\n\nБаланс лицевого счета: " + finalBalance +
+                    "\nНачислено бонусов: " + bonus + "\nБаланс бонусного счета: " + bonusBalance);
         } else {
-            System.out.println("Деньги зачислены!\nБаланс бонусного счета: " + bonusBalance);
+            System.out.println("Деньги зачислены!\n\nБаланс лицевого счета: " + finalBalance +
+                    "\nБаланс бонусного счета: " + bonusBalance);
         }
-
-        // Объявляете переменные для входных данных и
-        // параметров программы: начального счёта,
-        // суммы пополнения и тп
-
-        // Условным оператором проверяете превысила ли
-        // сумма пополнения порог и для этих двух разных
-        // сценариев рассчитываете сумму бонуса и выводите
-        // на экран.
-
-
     }
 }
