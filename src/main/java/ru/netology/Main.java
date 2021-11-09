d3768221e54ec379610a4296bb542953dd0728b5
Aleksey Alekseev <a.alekseev5743@yandex.ru>
2021-11-09 08:45:36 +0300

    commit 09.11.21

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -3,21 +3,18 @@ package ru.netology;
 public class Main {
     public static void main(String[] args) {
 
-        int customerBalance = 500; // баланс на счете клиента
-
-        int depositAmount = 100; // сумма пополнения
-        int bonus; // начисляемые бонусы
-        int bonusBalance = 7; // баланс бонусного счета
-        int finalBalance = customerBalance + depositAmount; //расчет баланса после пополнения
+        int customerAmountBalance = 100; // начальный баланс клиента
+        int depositAmount = 1100; // сумма пополнения
+        int bonus = 0; // бонусы
+        int finalAmountBalance; // итоговый баланс клиента
 
         if (depositAmount > 1000) {
             bonus = depositAmount / 100;
-            bonusBalance += bonus;
-            System.out.println("Деньги зачислены!\n\nБаланс лицевого счета: " + finalBalance +
-                    "\nНачислено бонусов: " + bonus + "\nБаланс бонусного счета: " + bonusBalance);
+            finalAmountBalance = customerAmountBalance + depositAmount + bonus;
         } else {
-            System.out.println("Деньги зачислены!\n\nБаланс лицевого счета: " + finalBalance +
-                    "\nБаланс бонусного счета: " + bonusBalance);
+            finalAmountBalance = customerAmountBalance + depositAmount;
         }
+        System.out.println("Деньги зачислены!\n\nБаланс лицевого счета: " + finalAmountBalance +
+                "\nБаланс бонусного счета: " + bonus);
     }
 }
